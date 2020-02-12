359cbe43c0ffd42513457884eab03815aa85b183
=Herve Yves <herventonga20@gmail.com>
2020-02-12 14:31:29 +0100

    Réalisation du compteur

---
+++ index.js
@@ -0,0 +1,21 @@
+
+sheep = [ 10, 17, 1, 15, 10, 22, 17, 1];
+let compteur = 0;
+
+  function findSheep(sheep) {
+      this.sheep.forEach(element =>{
+        if( sheep !== null && sheep !== undefined){
+          if(element === sheep) {
+            compteur ++
+          }
+        }
+      })
+      console.log(compteur + ' moutons');
+  }
+
+  function main(){
+    findSheep(17);
+  }
+
+  main();
+
