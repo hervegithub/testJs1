1089e91ab7952e4056f4dcc0cda161cbb83554e1
=Herve Yves <herventonga20@gmail.com>
2020-02-13 10:12:10 +0100

    Mise à jour de mon fichier

--- remove_caracter.js
+++ remove_caracter.js
@@ -1,9 +1,6 @@
-myString = "Ceci est une phrase";
 
-
-console.log('hallo');
-function removeChar() {
-    console.log(myString.substr(1, this.myString.length - 2));
+function removeChar(myString) {
+    console.log(myString.substr(1, myString.length - 2));
 }
 
-removeChar();
+removeChar("Ceci est une phrase");
